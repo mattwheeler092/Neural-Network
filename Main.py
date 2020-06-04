c88a076b069ac03d21fc0234e9e2a0fabf079b02
mattwheeler092 <mattwheeler092@googlemail.com>
2020-06-04 19:07:28 +0100

    Added Network class to implement feedforward.

--- Main.py
+++ Main.py
@@ -1,38 +1,49 @@
 import numpy as np 
 
+def relu(x):
+    return np.maximum(0,x)
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-test = 5
 
 class Layer:
     
-    def __init__(self, inputs, outputs, activation = None):
-        self.num_input = inputs
-        self.num_output = outputs
-        self.weights = np.random.rand(outputs, inputs)
-        self.bias = np.random.rand(outputs)
+    def __init__(self, inputs, outputs, activation):
+        self.weights = 2 * np.random.rand(outputs, inputs) - 1
+        self.bias = 2 * np.random.rand(outputs) - 1
         self.activation = activation
+        self.output_vals = None
+    
+    
+class Network:
+    
+    def __init__(self, network_layers):
+        for index, layer in enumerate(network_layers[:-1]):
+            if network_layers[index + 1].weights.shape[1] != layer.weights.shape[0]:
+                msg = 'Layers {} and {} have unequal output / input nodes'.format(index, index + 1)
+                raise ValueError (msg)
+        self.layers = network_layers
+        self.input = None
+        self.output = None
         
-    def feedforward(self, input):
-        if input.size != self.num_input:
-            raise ValueError ('Input array must contain {} elements.'.format(self.num_input))
-        layer_output = np.dot(self.weights, input) + self.bias
-        if self.activation is not None:
-            return self.activation(layer_output)
-        else:
-            return 1 / (1 + np.exp(-layer_output))
+    def feedforward(self, network_input):
+        self.input = network_input
+        layer_output = network_input
+        for layer in self.layers:
+            layer_output = layer.activation(np.dot(layer.weights, layer_output) + layer.bias)
+        self.output = layer_output
+        print(layer_output)
+      
+s = sigmoid  
         
+test = np.array([Layer(5,10,s),Layer(10,10,s),Layer(10,10,s),Layer(10,5,s)])
+ 
+test_network = Network(test)
+
+test_network.feedforward(np.array([1,2,3,4,5]))
 
 
-L1 = Layer(5,10,sigmoid)
-L2 = Layer(10,10,sigmoid)
-L3 = Layer(10,5,sigmoid)
 
-input = np.random.rand(5)
 
-FF1 = L1.feedforward(input)
-FF2 = L2.feedforward(FF1)
-FF3 = L3.feedforward(FF2)
 
-print(FF3)
