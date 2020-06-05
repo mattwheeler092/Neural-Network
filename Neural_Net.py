3f18cd37ec32bcbeefd9364e3d5a064900ff657c
mattwheeler092 <mattwheeler092@googlemail.com>
2020-06-05 23:04:52 +0100

    Feed forward function fixed

--- Neural_Net.py
+++ Neural_Net.py
@@ -14,23 +14,23 @@ class Network:
     
     def __init__(self, num_inputs, network_layers):
         self.network_layers = network_layers
-        self.network_state = [np.zeros((num_inputs,1))]
+        self.network_state = [np.zeros(num_inputs)]
         self.network_parameters = []
         for index, layer in enumerate(network_layers):
-            self.network_state.append(np.zeros((layer.num_nodes,1)))
+            self.network_state.append(np.zeros(layer.num_nodes))
             if index == 0:
                 self.network_parameters.append([2 * np.random.rand(layer.num_nodes,num_inputs) - 1,
-                                                2 * np.random.rand(layer.num_nodes,1) - 1])
+                                                2 * np.random.rand(layer.num_nodes) - 1])
             else:
                 self.network_parameters.append([2 * np.random.rand(layer.num_nodes, network_layers[index - 1].num_nodes) - 1,
-                                                2 * np.random.rand(1,layer.num_nodes) - 1])
+                                                2 * np.random.rand(layer.num_nodes) - 1])
             
     
     def feed_forward(self, network_input):
         self.network_state[0] = network_input
         for index, (weights, bias) in enumerate(self.network_parameters):
-            x = np.dot(weights, self.network_state[index]) + bias
-            self.network_state[index + 1] = self.network_layers[index].activation(np.dot(weights, self.network_state[index]) + bias)
+            activation_function = self.network_layers[index].activation
+            self.network_state[index + 1] = activation_function(np.dot(weights, self.network_state[index]) + bias)
         
         
 
@@ -51,18 +51,17 @@ def mean_squared_error(prediction, actual, differential = False):
         return np.sum(0.5 * np.power(prediction - actual, 2))
         
         
-L1 = Layer(10, sigmoid)
-L2 = Layer(10, sigmoid)
-output = Layer(5, sigmoid)
+
+output = Layer(1, sigmoid)
 
 
-network = Network(5, [L1,L2,output])
+network = Network(2, [output])
 
 
 print(network.network_parameters[0][0])
 print(network.network_parameters[0][1])
 print()
 
-network.feed_forward(np.array([1,1,1,1,1]))
+network.feed_forward(np.array([1,1]))
 
 print(network.network_state[-1])
