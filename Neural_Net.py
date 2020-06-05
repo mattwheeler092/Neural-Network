bbc284df012be6e5aaeddf6e394b16cbd57e30e3
mattwheeler092 <mattwheeler092@googlemail.com>
2020-06-05 22:53:45 +0100

    Can now feed forward through the network.

--- Neural_Net.py
+++ Neural_Net.py
@@ -1,5 +1,7 @@
 import numpy as np 
 
+np.random.seed(12345)
+
 
 class Layer:
     
@@ -10,33 +12,57 @@ class Layer:
         
 class Network:
     
-    def __init__(self, num_inputs, num_outputs, network_layers = []):
+    def __init__(self, num_inputs, network_layers):
         self.network_layers = network_layers
-        self.network_state = [np.zeros(num_inputs),np.zeros(num_outputs)]
-        if not network_layers:
-            self.network_parameters = [2 * np.random.rand(num_inputs,num_outputs) - 1,
-                                       2 * np.random.rand(1,num_outputs) - 1]
-        else:
-            self.network_parameters = []
-            for index, layer in enumerate(network_layers):
-                self.network_state.insert(-1, np.zeros(layer.num_nodes))
-                if index == 0:
-                    self.network_parameters.append([2 * np.random.rand(num_inputs,layer.num_nodes) - 1,
-                                                    2 * np.random.rand(1,layer.num_nodes) - 1])
-                else:
-                    self.network_parameters.append([2 * np.random.rand(network_layers[index - 1].num_nodes,layer.num_nodes) - 1,
-                                                    2 * np.random.rand(1,layer.num_nodes) - 1])
-            self.network_parameters.append([2 * np.random.rand(network_layers[-1].num_nodes,num_outputs) - 1,
-                                            2 * np.random.rand(1,num_outputs) - 1])
+        self.network_state = [np.zeros((num_inputs,1))]
+        self.network_parameters = []
+        for index, layer in enumerate(network_layers):
+            self.network_state.append(np.zeros((layer.num_nodes,1)))
+            if index == 0:
+                self.network_parameters.append([2 * np.random.rand(layer.num_nodes,num_inputs) - 1,
+                                                2 * np.random.rand(layer.num_nodes,1) - 1])
+            else:
+                self.network_parameters.append([2 * np.random.rand(layer.num_nodes, network_layers[index - 1].num_nodes) - 1,
+                                                2 * np.random.rand(1,layer.num_nodes) - 1])
             
+    
+    def feed_forward(self, network_input):
+        self.network_state[0] = network_input
+        for index, (weights, bias) in enumerate(self.network_parameters):
+            x = np.dot(weights, self.network_state[index]) + bias
+            self.network_state[index + 1] = self.network_layers[index].activation(np.dot(weights, self.network_state[index]) + bias)
+        
+        
 
 
-L1 = Layer(5,1)
 
-network = Network(5,5)
+  
+        
+def sigmoid(x, differential = False):
+    if differential:
+        return np.power(sigmoid(x),2) * np.exp(-x)
+    else:
+        return 1 / (1 + np.exp(-x))
+    
+def mean_squared_error(prediction, actual, differential = False):
+    if differential:
+        return prediction - actual
+    else:
+        return np.sum(0.5 * np.power(prediction - actual, 2))
+        
+        
+L1 = Layer(10, sigmoid)
+L2 = Layer(10, sigmoid)
+output = Layer(5, sigmoid)
 
-print(network.network_parameters)
+
+network = Network(5, [L1,L2,output])
 
 
+print(network.network_parameters[0][0])
+print(network.network_parameters[0][1])
+print()
 
+network.feed_forward(np.array([1,1,1,1,1]))
 
+print(network.network_state[-1])
