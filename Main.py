c642be229069affc635c107879a27473719a682b
mattwheeler092 <mattwheeler092@googlemail.com>
2020-06-05 19:43:25 +0100

    Now computes the average gradient value for a batch of data.

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 import numpy as np 
 
+np.random.seed(12345)
+
 class Layer:
     
     def __init__(self, inputs, outputs, activation):
         self.num_input = inputs
         self.num_output = outputs
         self.activation = activation
-        self.output_vals = None
     
 class Network:
     
-    def __init__(self, network_layers, cost_function):
+    def __init__(self, network_layers):
         self.network_variables = []
         self.network_state = []
-        self.cost_function = cost_function
         for index, layer in enumerate(network_layers):
             layer_weights = 2 * np.random.rand(layer.num_output, layer.num_input) - 1
             layer_bias = 2 * np.random.rand(layer.num_output) - 1
@@ -27,24 +27,45 @@ class Network:
         for index, (weights, bias) in enumerate(self.network_variables):
             self.network_state[index + 1] = self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias)
             
-    def compute_cost(self, actual_output):
-        return self.cost_function(self.network_state[-1], actual_output)
+    def compute_cost(self, actual_output, cost_function):
+        return cost_function(self.network_state[-1], actual_output)
    
-    def compute_gradient(self, actual_output):
+    def compute_gradient(self, batch_input, batch_output, cost_function):
         network_gradients = []
-        for index, (weights, bias) in reversed(list(enumerate(self.network_variables))):
-            
-            if index == len(self.network_variables) - 1:
-                sigma = np.multiply(self.cost_function(self.network_state[index + 1], actual_output, True), 
-                                    self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
-                network_gradients.append([np.dot(np.transpose([sigma]),[self.network_state[index]]), sigma])
+        for batch_index, output in enumerate(batch_output):
+            self.feed_forward(batch_input[batch_index])
+            print('Hello')
+            for index, (weights, bias) in reversed(list(enumerate(self.network_variables))):
                 
-            else:
-                sigma = np.multiply(np.dot(np.transpose(self.network_variables[index + 1][0]), network_gradients[0][1]), 
-                                    self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
-                network_gradients.insert(0, [np.dot(np.transpose([sigma]),[self.network_state[index]]), sigma]) 
+                if index == len(self.network_variables) - 1:
+                    sigma = np.multiply(cost_function(self.network_state[index + 1], output, True), 
+                                        self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
+                    if batch_index == 0:
+                        network_gradients.append([np.dot(np.transpose([sigma]),[self.network_state[index]]), sigma])
+                    elif batch_index == batch_input.shape[0] - 1:
+                        network_gradients[index][0] = (network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])) / batch_input.shape[0]
+                        network_gradients[index][1] = (network_gradients[index][1] + sigma) / batch_input.shape[0]
+                    else:
+                        network_gradients[index][0] = network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])
+                        network_gradients[index][1] = network_gradients[index][1] + sigma
+                    
+                else:
+                    if batch_index == 0:
+                        sigma = np.multiply(np.dot(np.transpose(self.network_variables[index + 1][0]), network_gradients[0][1]), 
+                                            self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
+                        network_gradients.insert(0, [np.dot(np.transpose([sigma]),[self.network_state[index]]), sigma]) 
+                    elif batch_index == batch_input.shape[0] - 1:
+                        sigma = np.multiply(np.dot(np.transpose(self.network_variables[index + 1][0]), network_gradients[index + 1][1]), 
+                                            self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
+                        network_gradients[index][0] = (network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])) / batch_input.shape[0]
+                        network_gradients[index][1] = (network_gradients[index][1] + sigma) / batch_input.shape[0]
+                    else:
+                        sigma = np.multiply(np.dot(np.transpose(self.network_variables[index + 1][0]), network_gradients[index + 1][1]), 
+                                            self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
+                        network_gradients[index][0] = network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])
+                        network_gradients[index][1] = network_gradients[index][1] + sigma
                 
-        print(network_gradients)
+            print(network_gradients[0][1])
                 
    
 
@@ -71,16 +92,15 @@ MSE = mean_squared_error
         
 test = np.array([Layer(5,10,s),Layer(10,10,s),Layer(10,10,s),Layer(10,5,s)])
  
-test_network = Network(test, MSE)
+test_network = Network(test)
 
-test_network.feed_forward(np.array([1,2,3,4,5]))
+x = np.array([np.array([1,2,3,4,5]),np.array([5,4,3,2,1])])
 
 #print(test_network.compute_cost(np.array([1,2,3,4,5])))
 
-test_network.compute_gradient(np.array([1,2,3,4,5]))
+test_network.compute_gradient(x, x, MSE)
 
-
-
+print(x.shape)
 
 
 
