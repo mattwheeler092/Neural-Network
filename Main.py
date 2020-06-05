864be5b4c52c3ddab7a1dda200301ed27828c92b
mattwheeler092 <mattwheeler092@googlemail.com>
2020-06-05 21:27:06 +0100

    Testing if it correctly optimises weights

--- Main.py
+++ Main.py
@@ -2,14 +2,21 @@ import numpy as np
 
 np.random.seed(12345)
 
+
+#======================================================================================================================================#
+
 class Layer:
     
     def __init__(self, inputs, outputs, activation):
         self.num_input = inputs
         self.num_output = outputs
         self.activation = activation
+        
+#======================================================================================================================================#
     
 class Network:
+    
+    #----------------------------------------------------------------------------------------------------------------------------------#
     
     def __init__(self, network_layers):
         self.network_parameters = []
@@ -22,10 +29,14 @@ class Network:
             self.network_state.append(np.zeros(layer.num_output))
         self.layers = network_layers
         
+    #----------------------------------------------------------------------------------------------------------------------------------#
+        
     def feed_forward(self, network_input):
         self.network_state[0] = network_input
         for index, (weights, bias) in enumerate(self.network_parameters):
             self.network_state[index + 1] = self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias)
+            
+    #----------------------------------------------------------------------------------------------------------------------------------#
             
     def compute_cost(self, batch_input, batch_output, cost_function):
         cost_value = 0
@@ -44,9 +55,11 @@ class Network:
                                         self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
                     if batch_index == 0:
                         network_gradients.append([np.dot(np.transpose([sigma]),[self.network_state[index]]), sigma])
+                        
                     elif batch_index == batch_input.shape[0] - 1:
                         network_gradients[index][0] = (network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])) / batch_input.shape[0]
                         network_gradients[index][1] = (network_gradients[index][1] + sigma) / batch_input.shape[0]
+                        
                     else:
                         network_gradients[index][0] = network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])
                         network_gradients[index][1] = network_gradients[index][1] + sigma
@@ -55,24 +68,29 @@ class Network:
                         sigma = np.multiply(np.dot(np.transpose(self.network_parameters[index + 1][0]), network_gradients[0][1]), 
                                             self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
                         network_gradients.insert(0, [np.dot(np.transpose([sigma]),[self.network_state[index]]), sigma]) 
+                        
                     elif batch_index == batch_input.shape[0] - 1:
                         sigma = np.multiply(np.dot(np.transpose(self.network_parameters[index + 1][0]), network_gradients[index + 1][1]), 
                                             self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
                         network_gradients[index][0] = (network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])) / batch_input.shape[0]
                         network_gradients[index][1] = (network_gradients[index][1] + sigma) / batch_input.shape[0]
+                        
                     else:
                         sigma = np.multiply(np.dot(np.transpose(self.network_parameters[index + 1][0]), network_gradients[index + 1][1]), 
                                             self.layers[index].activation(np.dot(weights, self.network_state[index]) + bias, True))
                         network_gradients[index][0] = network_gradients[index][0] + np.dot(np.transpose([sigma]),[self.network_state[index]])
                         network_gradients[index][1] = network_gradients[index][1] + sigma      
+                        
         return network_gradients
+    
+    #----------------------------------------------------------------------------------------------------------------------------------#
     
     def update_parameters(self, gradient_values, learning_rate):
         for index, (grad_weights, grad_bias) in enumerate(gradient_values):
             self.network_parameters[index][0] -= learning_rate * grad_weights
             self.network_parameters[index][1] -= learning_rate * grad_bias
             
-                
+#======================================================================================================================================#                
 
 
       
@@ -97,15 +115,20 @@ test = np.array([Layer(5,10,s),Layer(10,10,s),Layer(10,10,s),Layer(10,5,s)])
  
 test_network = Network(test)
 
-x = np.array([np.array([1,2,3,4,5]),np.array([5,4,3,2,1])])
+x = np.array([np.array([1,2,3,4,5])])
 
 #print(test_network.compute_cost(np.array([1,2,3,4,5])))
 
-y = test_network.compute_gradient(x, x, MSE)
+print(test_network.network_parameters[-1][1])
 
-print(y)
+for _ in range(10000):
+    y = test_network.compute_gradient(x, x, MSE)
+    test_network.update_parameters(y,1)
 
-
+print(test_network.network_parameters[-1][1])
+print()
+test_network.feed_forward(x[0])
+print(test_network.network_state[-1])
 
 
 
